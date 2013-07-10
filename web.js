ae802e57d4a4eab4940783c2d93e2dd071be4bef
Anirban Das <anirban25987@gmail.com>
2013-07-10 23:07:06 +0000

    anirban

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
        var file="index.html";
-	   var temp= fs.readSync(file);
+	   var temp= fs.readFileSync(file);
 	   response.send('hello parallel worlds!!');
 });
 var port = process.env.PORT || 5000;
