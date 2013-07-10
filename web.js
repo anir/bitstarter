daa4fcc28f1c8d5e059a7e27693aa7ca82e862f9
Anirban Das <anirban25987@gmail.com>
2013-07-10 21:15:50 +0000

    Saving web.js

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-
+var buffer = new Buffer(16);
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    buffer= fs.readSync('index.html');
+    response.send(buffer.toString);
 });
 
 var port = process.env.PORT || 5000;
