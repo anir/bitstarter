ee85d333ec561999158fe6b766a7db549bc7242d
Anirban Das <anirban25987@gmail.com>
2013-07-10 22:44:36 +0000

    abc

--- web.js
+++ web.js
@@ -1,9 +1,20 @@
-var express = require('express');
+var fs = require('express');
+var fs = require('fs');
 var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
-       response.send('hello parallel worlds!!');
+       var file="index.html";
+	   var temp= fs.readSync(file);
+	   response.send('hello parallel worlds!!');
 });
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+
+
+
+
+
+
